fecc7f4558e3385329eb3d50a1aba1728e608912
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-07-15 21:55:45 -0400

    meme

--- server.rb
+++ server.rb
@@ -19,6 +19,14 @@ get "/" do
 
    "Your stock is at $ #{stock_price3}"
 
+   # These code snippets use an open-source library.
+response = HTTParty.get "https://ronreiter-meme-generator.p.mashape.com/meme?bottom=is+DOPE&font=Impact&font_size=50&meme=Condescending+Wonka&top=MY+WEBSITE",
+  headers:{
+    "X-Mashape-Key" => "P0fN9NWdbCmshpsgvVM8j3ARfYkyp18A8KyjsnKsbe0vNFrD52"
+  }
+
+  response
+
 end
 
 # def hello
