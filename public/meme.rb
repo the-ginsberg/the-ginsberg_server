258497a7c4f800f3e3caeeb35b41f04ae28a4ac8
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-07-19 18:54:21 -0400

    Webpage going live

--- public/meme.rb
+++
@@ -1,6 +0,0 @@
-response = HTTParty.get "https://ronreiter-meme-generator.p.mashape.com/meme?bottom=is+DOPE&font=Impact&font_size=50&meme=Condescending+Wonka&top=MY+WEBSITE",
-  headers:{
-    "X-Mashape-Key" => "P0fN9NWdbCmshpsgvVM8j3ARfYkyp18A8KyjsnKsbe0vNFrD52"
-  }
-
-  response
--- server.rb
+++ server.rb
@@ -1,49 +1,6 @@
 require "sinatra"
-# require "httparty"
-# require "nokogiri"
 
 get "/" do
-
-  # puts "What stock would you like to look up? Please enter the stock symbol."
-
-  # ticker = gets.chomp
-
-  # url3 = "http://finance.yahoo.com/q?s=aapl"
-  #
-  # response3 = HTTParty.get url3
-  #
-  # dom3 = Nokogiri::HTML(response3.body)
-  #
-  # stock_price3 = dom3.xpath('//*[@id="yfs_l84_aapl"]').first.content
-  #
-  #
-  #  "Your stock is at $ #{stock_price3}"
-
-   # These code snippets use an open-source library.
-# response = HTTParty.get "https://ronreiter-meme-generator.p.mashape.com/meme?bottom=is+DOPE&font=Impact&font_size=50&meme=Condescending+Wonka&top=MY+WEBSITE",
-#   headers:{
-#     "X-Mashape-Key" => "P0fN9NWdbCmshpsgvVM8j3ARfYkyp18A8KyjsnKsbe0vNFrD52"
-#   }
-#
-#   response
-
-"Jeffs Website!"
+  file.read(File.join('public', 'index.html'))
 
 end
-
-# def hello
-#   "Welome to Jeff's first website!"
-# end
-#
-#
-# get "/" do
-#   hello
-# end
-#
-# get "/blah" do
-#   hello
-# end
-#
-# get "/sinatra" do
-#   "Hello Sinatra!"
-# end
